527f30aa729a607618eebbda73828986c4fb43dd
Carlos-Alvarado <alvaradocarlosluis97@gmail.com>
2021-10-12 20:39:33 -0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Material App2',
       home: Scaffold(body: LoginPage()),
     );
   }
