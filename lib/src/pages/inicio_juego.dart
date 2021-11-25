75f2fe03bfbd64d4ada037b8d4729e5c71e50272
Marga <margaritazerpa18@gmail.com>
2021-11-25 01:03:23 -0300

    cambio en tarjetas

--- lib/src/pages/inicio_juego.dart
+++ lib/src/pages/inicio_juego.dart
@@ -61,16 +61,24 @@ class InicioJuego extends StatefulWidget {
 
   Widget _swiperTarjetas(){
     final _screenSize= MediaQuery.of(context).size;
-    List<String> images = [
-    "https://www.patagonline.com/wp-content/uploads/2020/10/Screenshot-2020-10-20-at-14.46.48.jpg",
-    "https://viajerosocultos.com/wp-content/uploads/2021/05/20210515_113848_HDR-scaled.jpg",
-    "https://www.periodicolea.com.ar/wp-content/uploads/2019/02/pag-1-Jueves-de-Compadres.jpg",
-    "https://www.nordic-travel.com.ar/es/wp-content/uploads/2020/06/humahuaca-galeria-5.jpg",
-    "https://www.quebradadehumahuaca.com/info-gral/imagenes/quebrada-de-humahuaca-pucara-tilcara.jpg",
-    "https://tripin.travel/wp-content/uploads/2020/05/Purmamarca-Jujuy.jpg",
-    "https://somosjujuy-wordpress.s3.amazonaws.com/wp-content/uploads/2021/01/06171110/carnaval.jpg",
-  ];
-    return CardSwiper(lugares:images);
+    List<String> imgList = [
+      "https://promociones-aereas.com.ar/wp-content/uploads/2019/09/10-lugares-en-jujuy-que-no-podes-perderte-los-colorados-cerro-7-colores-purmamarca-holding-world-1-1024x512.jpg.webp",
+      "https://media.viajando.travel/p/a30dd40bd762703afe2526904b7ed794/adjuntos/255/imagenes/000/348/0000348488/jujuy-salinas-grandesjpg.jpg",
+      "https://promociones-aereas.com.ar/wp-content/uploads/2019/09/10-lugares-en-jujuy-que-no-podes-perderte-en-jujuy-cerro-monta%C3%B1a-chani-1024x681.jpg.webp",
+      "https://media.viajando.travel/p/5630ab283257e18d749bfc4fb0633e63/adjuntos/255/imagenes/000/348/0000348478/jujuy-laguna-pozuelosjpg.jpg",
+      "https://media.viajando.travel/p/6732af207ecea5fec7733259663cbd82/adjuntos/255/imagenes/000/348/0000348458/jujuy-barrancasjpg.jpg",
+      "https://www.patagonline.com/wp-content/uploads/2020/10/Screenshot-2020-10-20-at-14.46.48.jpg",
+      "https://viajerosocultos.com/wp-content/uploads/2021/05/20210515_113848_HDR-scaled.jpg",
+      "https://www.periodicolea.com.ar/wp-content/uploads/2019/02/pag-1-Jueves-de-Compadres.jpg",
+      "https://www.nordic-travel.com.ar/es/wp-content/uploads/2020/06/humahuaca-galeria-5.jpg",
+      "https://www.quebradadehumahuaca.com/info-gral/imagenes/quebrada-de-humahuaca-pucara-tilcara.jpg",
+      "https://tripin.travel/wp-content/uploads/2020/05/Purmamarca-Jujuy.jpg",
+      "https://somosjujuy-wordpress.s3.amazonaws.com/wp-content/uploads/2021/01/06171110/carnaval.jpg",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/f6/70/ea/inca-cueva.jpg?w=1200&h=-1&s=1",
+      "https://media.todojujuy.com/p/677000a117e3dc0611d6550112cbc802/adjuntos/227/imagenes/003/194/0003194169/quebrada-las-senoritas-foto-porafuerita.png?2021-10-06-11-09-29",
+    ];
+    
+    return CardSwiper(lugares:imgList);
   }
 
   Widget _buttonJuego() {
--- lib/src/widgets/card_swiper.dart
+++ lib/src/widgets/card_swiper.dart
@@ -1,11 +1,13 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_application_1/src/pages/inicio_juego.dart';
+int activeIndex = 0;
 class CardSwiper extends StatelessWidget {
   
   final List <String> lugares;
   const CardSwiper({Key? key, required this.lugares}): super(key: key);//recibir lista
-
+  
   @override
   Widget build(BuildContext context) {
 
@@ -13,8 +15,35 @@ class CardSwiper extends StatelessWidget {
 
     return Container(
       padding: const EdgeInsets.only(top:10.0),
+      child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              items: lugares
+                  .map((item) => Container(
+                        child: Center(
+                          child: Image.network(
+                            item,
+                            fit: BoxFit.cover,
+                            width: 20000,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                autoPlay: true,
+                height: 300,
+                reverse: false,
+                autoPlayInterval: Duration(seconds: 2),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+          ],
+        ),
            
-      child:Swiper(
+      /*child:Swiper(
           layout: SwiperLayout.STACK,
           itemWidth:_screenSize.width *0.7,
           itemHeight: _screenSize.height* 0.5,
@@ -24,7 +53,7 @@ class CardSwiper extends StatelessWidget {
               child:Image.network(lugares[index], fit: BoxFit.cover,));   
          },
           itemCount:lugares.length,
-     ),
+     ),*/
     );
   }
 }
